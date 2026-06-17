d0a17bb1b0ecebd9dfd6f7939940da6d3fc719ba
alessandrosebastianelli <alessandroseba1995@gmail.com>
2026-06-17 09:52:44 +0200

    adding lib publishing support

--- build_dist.py
+++ build_dist.py
@@ -6,11 +6,11 @@ import os
 
 
 # Clean dist folder
-shutil.rmtree('./build/', ignore_errors=True)
-shutil.rmtree('./dist/', ignore_errors=True)
+shutil.rmtree('./build/',         ignore_errors=True)
+shutil.rmtree('./dist/',          ignore_errors=True)
 shutil.rmtree('./aule.egg-info/', ignore_errors=True)
 
 # Build distribution
 os.system("python build_doc.py")
-os.system("python -m build")
+os.system("python setup.py sdist bdist_wheel")
 os.system("python -m twine upload dist/* --verbose")
